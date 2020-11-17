b87063590fcb0b397ab58fe1310c1834e102fd24
André Passos <52414263+andregpassos@users.noreply.github.com>
2020-11-17 15:25:16 -0300

    Update UserControl.java

--- src/business/control/UserControl.java
+++ src/business/control/UserControl.java
@@ -1,11 +1,33 @@
 package business.control;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.io.FileReader;
+import java.io.BufferedReader;
 
 public class UserControl {
     ArrayList<String> users;
-    //talvez seja melhor passar o nome do arquivo no construtor, ler o arquivo, extrair a lista de usuarios e atribuir a users.
-    public UserControl(){
-        users = new ArrayList<String>();
+    
+    //construtor a partir de um ArrayList
+    public UserControl(ArrayList<String> users){
+        this.users = users;
+    }
+
+    //contrutor a partir de um arquivo
+    public UserControl(String caminhoArquivo) { //caminhoArquivo seria algo do tipo "D:\\arquivo.txt"
+        try {
+            FileReader abrirArq = new FileReader(caminhoArquivo);
+            BufferedReader lerArq = new BufferedReader(abrirArq);
+
+            while (lerArq.ready()) {
+                String linha = lerArq.readLine();
+                this.users.add(linha);
+            }
+
+            abrirArq.close();
+            lerArq.close();
+        } catch (IOException e) {
+            System.out.printf("Erro na abertura do arquivo: %s.\n", e.getMessage());
+        }
     }
 
     public void deleteUser(String login){}
